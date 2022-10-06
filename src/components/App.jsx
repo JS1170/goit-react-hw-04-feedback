31298c2d8340aece9b4020478121417c2241a4d6
Yevgen Savchuk <107425775+JS1170@users.noreply.github.com>
2022-10-06 17:43:44 +0300

    change first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,72 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onClickBtn = event => {
+    switch(event) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  onClickBtn = value => {
-    this.setState(prevState => ({
-      [value]: prevState[value] + 1,
-    }));
-    // console.log(value);
+  // onClickBtn = value => {
+  //   this.setState(prevState => ({
+  //     [value]: prevState[value] + 1,
+  //   }));
+  // };
+
+  const totalCount = () => {
+    return good + neutral + bad;
   };
 
-  totalCount = () => {
-    const allValues = Object.values(this.state);
-    return allValues.reduce((acc, value) => {
-      return acc + value;
-    }, 0);
+  const positiveCount = () => {
+    return Math.round((good / totalCount()) * 100);
   };
 
-  positiveCount = () => {
-    const { good } = this.state;
-    return Math.round((good / this.totalCount()) * 100);
-  };
+  // const { good, neutral, bad } = this.state;
+  // const total = totalCount();
+  // const count = positiveCount();
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.totalCount();
-    const count = this.positiveCount();
-
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            // options={['good', 'neutral', 'bad']}
-            options={this.state}
-            onLeaveFeedback={this.onClickBtn}
-          />
-        </Section>
-        <Section title="Statistics">
-          {total ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={count}
-            ></Statistics>
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onClickBtn}
+        />
+      </Section>
+      <Section title="Statistics">
+        {totalCount() ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalCount()}
+            positivePercentage={positiveCount()}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
 }
-
-
-export default App;
--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 import css from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  const objectKeys = Object.keys(options);
+  // const objectKeys = Object.keys(options);
   console.log(options);
   return (
     <div className={css.Counter__controls}>
-      {objectKeys.map(option => (
+      {options.map(option => (
         <button
           className={css.counter__btn}
           type="button"
@@ -24,7 +24,7 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 
 FeedbackOptions.propTypes = {
   options: PropTypes.objectOf(
-    PropTypes.number.isRequired
+    PropTypes.string.isRequired
   ),
   onLeaveFeedback: PropTypes.func.isRequired,
 };
